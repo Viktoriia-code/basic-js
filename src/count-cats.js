f8c4965cb54f60bfa0ff7d2dc7e1daf05a3713af
Viktoriia <beloborodova.viktoriia@gmail.com>
2023-12-14 17:49:06 +0200

    feat: added tasks

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrix) {
+  let cats = 0;
+  matrix.forEach(row => {
+    row.forEach(element => {
+      if (element === '^^') {
+        cats++;
+      }
+    });
+  });
+  return cats;
 }
 
 module.exports = {
--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,14 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (!Array.isArray(members) || members.length === 0) {
+    return false;
+  }
+  const dreamTeamChars = members
+    .filter(member => typeof member === 'string')
+    .map(member => member.trim()[0].toUpperCase());
+  return dreamTeamChars.sort().join('');
 }
 
 module.exports = {
